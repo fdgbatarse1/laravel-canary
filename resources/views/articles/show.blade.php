22f67155a8ca0ffc894e758bc6d0021a83269a35
fdgbatarse1 <fdgbatarse@gmail.com>
2025-11-14 18:55:04 +0100

    ✨ Update article show view to conditionally display comments; show a message when no comments are present.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,9 +2,13 @@
     <div>written by {{$article->author->name}}</div>
     {{$article->content}}
     <h2 class="font-bold mb-2 mt-8">Comments</h2>
-    <ul class="list-disc pl-4">
-        @foreach($article->comments as $comment)
-            <li>{{$comment->content}}</li>
-        @endforeach
-    </ul>
+    @if($article->comments->isNotEmpty())
+        <ul class="list-disc pl-4">
+            @foreach($article->comments as $comment)
+                <li>{{$comment->content}}</li>
+            @endforeach
+        </ul>
+    @else
+        No comments yet
+    @endif
 </x-site-layout>
