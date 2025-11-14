3d8547cd0b187b928a1335bfc514e62ad0b3bc1a
fdgbatarse1 <fdgbatarse@gmail.com>
2025-11-14 21:07:45 +0100

    ✨ Update ArticleFactory to include is_published field and modify articles migration to make content nullable.

--- database/migrations/2025_11_14_145017_create_articles_table.php
+++ database/migrations/2025_11_14_145017_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('content');
+            $table->text('content')->nullable();
             $table->foreignId('author_id');
             $table->timestamps();
         });
