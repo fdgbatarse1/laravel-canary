7a4efad1dc9e7099f9b35fb2c64beb7306ebfbf3
fdgbatarse1 <fdgbatarse@gmail.com>
2025-11-16 12:19:59 +0100

    ✨ Introduce FormText component for reusable text input fields in forms, and update category create/edit views to utilize this new component for improved code organization and consistency.

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,11 +1,7 @@
 <x-site-layout title="Create a new category">
     <form action="/categories" method="post">
         @csrf
-        <label>Category:</label>
-        <input type="text" name="name" placeholder="Add a category name" value="{{old('name')}}" class="border @error('name') border-red-500 @else border-black @enderror">
-        @error('name')
-            <div class="text-red-500">{{$message}}</div>
-        @enderror
+        <x-form-text name="name" label="Category" placeholder="Add a category name" />
         <div class="mt-4">
             <button class="bg-gray-200 p-2" type="submit">Create</button>
         </div>
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -2,9 +2,7 @@
     <form action="/categories/{{$category->id}}" method="POST">
         @csrf
         @method('PUT')
-        <label>Category:</label>
-        <input type="text" name="name" placeholder="Add a category name" value="{{$category->name}}"
-            class="border border-black">
+        <x-form-text name="name" label="Category" placeholder="Add a category name" value="{{$category->name}}" />
         <div class="mt-4">
             <button class="bg-gray-200 p-2" type="submit">Update</button>
         </div>
